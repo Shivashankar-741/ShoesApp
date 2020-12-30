34da683ba96431b6a76fa347ce7849df9b232305
Shivashankar-gitH <shiva1894300@gmail.com>
2020-12-30 14:36:10 +0530

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Header.css";
+import { FaShoppingCart } from "react-icons/fa";
+
+const handler = () => {
+	console.log("cllicked");
+};
 
 const Header = () => {
 	return (
 		<div className="Header">
 			<h1 className="Header--tag">Steps</h1>
-			<div className="Header--cart">cart</div>
+			<div className="Header--cart" onClick={handler}>
+				<FaShoppingCart className="Header--cart__logo" />
+				<span className="Header--cart--counter">0</span>
+			</div>
 		</div>
 	);
 };
