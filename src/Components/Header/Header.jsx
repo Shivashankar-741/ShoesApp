30f064ec5b8ba4ce357a906b91d93b830ab3bf4e
Shivashankar-gitH <shiva1894300@gmail.com>
2020-12-30 13:02:54 +0530

    initializing the components

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+	return (
+		<div className="Header">
+			<h1 className="Header--tag">Steps</h1>
+			<div className="Header--cart">cart</div>
+		</div>
+	);
+};
+
+export default Header;
